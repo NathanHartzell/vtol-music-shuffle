09a51e1b5f26820d7fb9bdb24081355ba5ca3318
nhartzel <nhartzel@uncc.edu>
2024-09-19 21:58:54 -0400

    reads music directory from file

--- vtol_mp3_shuffle.cpp
+++ vtol_mp3_shuffle.cpp
@@ -2,12 +2,14 @@
 #include<string.h>
 #include<iostream>
 #include<filesystem>
+#include<fstream>
 
 using namespace std;
 
 namespace fs = std::filesystem;
 
-#define FILEPATH "{ FILEPATH }"
+#define FILEPATH "/Users/nathanhartzell/Desktop/vtol_mp3_stream copy"
+#define IMPORT_PATH "dir_path.txt"
 #define PREFIX "vtol_"
 
 // @return true if file ends with .mp3
@@ -16,6 +18,22 @@ bool is_mp3(std::string filename) {
 }
 
 int main() {
+
+    std::ifstream file(IMPORT_PATH);
+    if (!file) {
+        std::cerr << "Could not open the file: " << IMPORT_PATH << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    if (std::getline(file, line)) {
+        std::cout << "Read line: " << line << std::endl;
+    } else {
+        std::cerr << "Could not read a line from the file." << std::endl;
+    }
+
+    file.close();
+
     std::vector<std::string> track_list;
 
     // iterate over song directory and store all filenames
@@ -38,6 +56,9 @@ int main() {
             ++i;
         } else { track_list.erase(track_list.begin() + i); }
     }
+
+    // shuffle track list
+
 
 
     return 0;
