939edf4994a0395900e998148b8f6d5d06c66f74
nhartzel <nhartzel@uncc.edu>
2024-09-19 22:18:18 -0400

    changed from removing non-mp3s to only adding mp3s

--- vtol_mp3_shuffle.cpp
+++ vtol_mp3_shuffle.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 namespace fs = std::filesystem;
 
-#define FILEPATH "/Users/nathanhartzell/Desktop/vtol_mp3_stream copy"
+//#define FILEPATH "/Users/nathanhartzell/Desktop/vtol_mp3_stream copy"
 #define IMPORT_PATH "dir_path.txt"
 #define PREFIX "vtol_"
 
@@ -25,9 +25,9 @@ int main() {
         return 1;
     }
 
-    std::string line;
-    if (std::getline(file, line)) {
-        std::cout << "Read line: " << line << std::endl;
+    std::string vtol_music_dir;
+    if (std::getline(file, vtol_music_dir)) {
+        std::cout << "Read line: " << vtol_music_dir << std::endl;
     } else {
         std::cerr << "Could not read a line from the file." << std::endl;
     }
@@ -37,28 +37,28 @@ int main() {
     std::vector<std::string> track_list;
 
     // iterate over song directory and store all filenames
-    for (const auto& entry : fs::directory_iterator(FILEPATH)) {
-        track_list.push_back(entry.path().filename().string());
+    for (const auto& entry : fs::directory_iterator(vtol_music_dir)) {
+        
+        // store filenames ending in .mp3
+        if (is_mp3(entry.path().filename().string()))
+            track_list.push_back(entry.path().filename().string());
     }
 
     // iterate over stored list of filenames, removing non-mp3s
-    for (int i = 0; i < track_list.size();) {
-        if (is_mp3(track_list[i])){
+    for (int i = 0; i < track_list.size(); i++) {
+        // check if file has prepended "vtol_" prefix
+        // allows user to add new files without manually adding prefix
+        if (track_list[i].substr(0, 5) == PREFIX) { 
 
-            // check if file has prepended "vtol_" prefix
-            // allows user to add new files without manually adding prefix
-            if (track_list[i].substr(0, 5) == PREFIX) { 
-
-                // removes prefix from filenames that have it
-                track_list[i] = track_list[i].substr(8, track_list[i].size()); 
-                std::cout << track_list[i] << std::endl;
-            } 
-            ++i;
-        } else { track_list.erase(track_list.begin() + i); }
+            // removes prefix from filenames that have it
+            track_list[i] = track_list[i].substr(8, track_list[i].size()); 
+            std::cout << track_list[i] << std::endl; // remove me
+        }
     }
 
-    // shuffle track list
+    // TODO: shuffle track list
 
+    // TODO: rename files in their random order
 
 
     return 0;
